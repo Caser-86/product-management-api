a717f44619fd4f44d863549ea31bd615fef4b6d1
Caser-86 <776706958@qq.com>
2026-04-10 18:57:40 +0800

    feat: add core product persistence model

---
+++ backend/src/main/java/com/example/ecommerce/product/domain/ProductSpuRepository.java
@@ -0,0 +1,6 @@
+package com.example.ecommerce.product.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductSpuRepository extends JpaRepository<ProductSpuEntity, Long> {
+}
